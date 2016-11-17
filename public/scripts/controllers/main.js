8b8407376758faecfb8e1b43145fbaf3d3e56340
Uros Radovanovic <radovanovic.uros92@gmail.com>
2016-11-17 18:44:19 +0100

    Build: Failing. Connected with Ethereum blockchain - developed authorization, managed to send transaction using web interface but events are not emmited to blockchain - after deploying new contract - need to create new solidity contracts.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -9,15 +9,67 @@
  */
 angular.module('bleaApp')
   .controller('MainCtrl', ['$rootScope', '$scope', '$q', 'emailService', function ($rootScope, $scope, $q, emailService) {
-    $rootScope.emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    $scope.sendEmail = function () {
-      var outgoingEmail = $scope.outgoingEmail;
-      emailService.sendEmail(outgoingEmail)
-        .then(function(res) {
-          console.log(res);
-        });
+
+    $scope.connected = true;
+
+    if(typeof web3 === 'undefined')
+        web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:4000'));
+
+    if (typeof web3 !== 'undefined') {
+  		var abi = web3.eth.contract([ { "constant": false, "inputs": [], "name": "withdraw", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "EmailAddress", "type": "string" }, { "name": "Subject", "type": "string" }, { "name": "Message", "type": "string" } ], "name": "SendEmail", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "EmailAddress", "type": "string" }, { "indexed": false, "name": "Subject", "type": "string" }, { "indexed": false, "name": "Message", "type": "string" } ], "name": "EmailSent", "type": "event" } ]);
+  		var myContract = abi.at('0x8038FC5f3DdDF44A53b9772925546EdFAcaE503F');
+  		var events = myContract.allEvents({fromBlock: 0, toBlock:'latest'});
+      console.log(events)
+
+      if (!web3.eth.accounts[0]) {
+        $scope.connected = false;
+        console.log('not connected to blockchain!')
+      } else {
+        web3.eth.getBalance(web3.eth.accounts[0],function(err, valuereturned) {
+          if(valuereturned<1000000000000000) {
+            $scope.insufficientFunds = true;
+          }
+        })
+    		$scope.messages = [];
+        $scope.eventData = [];
+    		// watch for changes
+    		events.watch(function(error, event) {
+          console.log(error, event)
+          if (!error) {
+            $scope.eventData.push(event);
+            event.args.message = web3.toAscii(event.args.message);
+            console.log($scope.eventData);
+            $scope.$apply();
+        }
+      });
     }
 
 
-  	console.log($scope);
+
+    $rootScope.emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    $scope.sendEmail = function () {
+
+
+      var authorizedAccount = web3.eth.accounts[0],
+              outgoingEmail = $scope.outgoingEmail,
+                      email = outgoingEmail.emailTo,
+                    subject = outgoingEmail.emailSubject,
+                    message = outgoingEmail.emailBody,
+          SendEmailContract = web3.eth.contract([ { "constant": false, "inputs": [], "name": "withdraw", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "EmailAddress", "type": "string" }, { "name": "Subject", "type": "string" }, { "name": "Message", "type": "string" } ], "name": "SendEmail", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "EmailAddress", "type": "string" }, { "indexed": false, "name": "Subject", "type": "string" }, { "indexed": false, "name": "Message", "type": "string" } ], "name": "EmailSent", "type": "event" } ]).at('0x9ee78C86aC32518cBc83d3975EDe44108f04B818');
+
+      console.log(email, subject, message);
+
+      $q.resolve(SendEmailContract.SendEmail(authorizedAccount, email, subject, message, {from: authorizedAccount, gas: 100000})).then(function (err, address) {
+          if (err){
+            console.log("Aaaaaaaaw snap! You dun goofed! error: " + err)
+          } else {
+            console.log("Great success!!! :Borat Disco Dance: address - " + address)
+          }
+      });
+  }
+
+
+
+  }
+
   }]);
